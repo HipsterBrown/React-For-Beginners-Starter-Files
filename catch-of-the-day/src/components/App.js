dfadc4690d424b00550ab9dfd9ad79013312a569
Nick Hehr <nicholas.hehr@betterment.com>
2019-07-31 17:04:59 -0400

    completed

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 // 1. create Fish component
 // 2. list using Object.key(fishes).map
@@ -28,12 +29,17 @@ const App = () => {
     setFishes(sampleFishes);
   }, []);
 
-  console.log(fishes);
-
   return (
     <div className="catch-of-the-day">
       <div className="menu">
         <Header tagline="Fresh Seafood Market" age={100} />
+        <ul>
+          {Object.keys(fishes).map(key => (
+            <li>
+              <Fish {...fishes[key]} />
+            </li>
+          ))}
+        </ul>
       </div>
       <Order />
       <Inventory addFish={addFish} loadSampleFishes={loadSampleFishes} />
--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -1,2 +1,17 @@
+import React from 'react';
 // 1. create Fish component
 // 2. display div.menu-fish > img[src=image] + (h3.fish-name > span.price) + p + button
+
+const Fish = ({desc, image, name, price}) => (
+  <div class="menu-fish">
+    <img src={image} alt={name} />
+    <h3 className="fish-name">
+      {name}
+      <span className="price">{price}</span>
+    </h3>
+    <p>{desc}</p>
+    <button>Add to Order</button>
+  </div>
+);
+
+export default Fish;
