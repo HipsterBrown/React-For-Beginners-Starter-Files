8aa65b747febc6675ff0408f24ceb1f28ae6b436
Nick Hehr <nicholas.hehr@betterment.com>
2019-07-31 00:16:09 -0400

    setup displaying order state

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,4 +1,8 @@
-import React from "react";
+import React from 'react';
+
+// 1. convert to FC
+// 2. use fishes, order props to derive total, display items
+// 3. render items with availability in mind: {count} lbs {name}\n{price + count}
 
 class Order extends React.Component {
   render() {
