2ad82ed73d2d88820650ab56ea61492ef2767f87
Nick Hehr <nicholas.hehr@betterment.com>
2019-07-31 16:15:56 -0400

    completed

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,26 +2,34 @@ import React from 'react';
 import {getFunName} from '../helpers';
 
 // 1. convert StorePicker to Functional Component
-// 2. overview of Hooks: https://reactjs.org/docs/hooks-intro.html
-// 3. overview of useCallback
-// 4. create goToStore callback for form submission
-// 5. talk about history.push from react-router
+// 2. overview of event handling in React
+// 3. overview of Hooks: https://reactjs.org/docs/hooks-intro.html
+// 4. overview of useCallback
+// 5. create goToStore callback for form submission
+// 6. talk about history.push from react-router
 
-class StorePicker extends React.Component {
-  render() {
-    return (
-      <form className="store-selector">
-        <h2>Please Enter A Store</h2>
-        <input
-          type="text"
-          required
-          placeholder="Store Name"
-          defaultValue={getFunName()}
-        />
-        <button type="submit">Visit Store →</button>
-      </form>
-    );
-  }
-}
+const StorePicker = ({history}) => {
+  const goToApp = event => {
+    event.preventDefault();
+
+    const {funName} = event.currentTarget.elements;
+
+    history.push(`/store/${funName.value}`);
+  };
+
+  return (
+    <form className="store-selector" onSubmit={goToApp}>
+      <h2>Please Enter A Store</h2>
+      <input
+        type="text"
+        required
+        name="funName"
+        placeholder="Store Name"
+        defaultValue={getFunName()}
+      />
+      <button type="submit">Visit Store →</button>
+    </form>
+  );
+};
 
 export default StorePicker;
